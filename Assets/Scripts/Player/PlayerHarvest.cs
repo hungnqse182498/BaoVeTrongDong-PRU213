dc3418b64d7c024e5b3252cdb27651fb9653193f
hungnqse182498 <hungnqse182498@fpt.edu.vn>
2026-01-31 13:28:54 +0700

    init

---
+++ Assets/Scripts/Player/PlayerHarvest.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Used to harvest fruits from <see cref="BushFruits"/>
+/// </summary>
+public class PlayerHarvest : MonoBehaviour
+{
+    [SerializeField] float harvestRadius;
+    [SerializeField] float harvestTime;
+    [SerializeField] LayerMask bushesMask;
+
+    PlayerMovement playerMovement;
+    PlayerBackpack backpack;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        backpack = GetComponent<PlayerBackpack>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Space))
+        {
+            TryHarvestClose();
+        }
+    }
+
+    /// <summary>
+    /// Harvesting is done in 2 parts: first, it tries to harvest really close bushes to guarantee that if the player harvests near 2 bushes,
+    /// it will always harvest the closest one.
+    /// <para>Then, if nothing was harvested in a close radius, it tries to harvest in the normal radius with <see cref="TryHarvestFruit"/></para>
+    /// </summary>
+    void TryHarvestClose()
+    {
+        Collider2D hit =
+        Physics2D.OverlapCircle(transform.position, harvestRadius / 2, bushesMask);
+
+        if (hit != null)
+        {
+            BushFruits bush = hit.GetComponent<BushFruits>();
+            if (bush.HasFruits())
+            {
+                audioSource.Play();
+                playerMovement.HarvestStopMovement(harvestTime);
+                backpack.AddFruits(bush.HarvestFruit());
+            }
+        }
+        else
+        {
+            TryHarvestFruit();
+        }
+    }
+    void TryHarvestFruit()
+    {
+        Collider2D hit =
+        Physics2D.OverlapCircle(transform.position, harvestRadius,bushesMask);
+
+        if(hit != null)
+        {
+            BushFruits bush = hit.GetComponent<BushFruits>();
+            if (bush.HasFruits())
+            {
+                audioSource.Play();
+                playerMovement.HarvestStopMovement(harvestTime);
+                backpack.AddFruits(bush.HarvestFruit());
+            }
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, harvestRadius);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Component responsible of the player movement and managing the <see cref="SpriteRenderer.flipX"/> property
+/// </summary>
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+
+    new Rigidbody2D rigidbody;
+    Vector2 normVector;
+    SpriteRenderer sprite;
+
+    float timer;
+    bool harvesting;
+    
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if(Time.time > timer)
+        {
+            harvesting = false;
+        }
+        FlipSprite();
+    }
+
+    private void FlipSprite()
+    {
+        if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            sprite.flipX = false;
+        }
+        else if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            sprite.flipX = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (harvesting)
+        {
+            rigidbody.velocity = Vector2.zero;
+        }
+        else
+        {
+            normVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            if(normVector.sqrMagnitude > 1)
+            {
+                normVector = normVector.normalized;
+            }
+            rigidbody.velocity = new Vector2(normVector.x * movementSpeed,normVector.y * movementSpeed);
+        }
+    }
+
+    public void HarvestStopMovement(float time)
+    {
+        harvesting = true;
+        timer = Time.time + time;
+    }
+
+    public bool IsHarvesting()
+    {
+        return harvesting;
+    }
+}
